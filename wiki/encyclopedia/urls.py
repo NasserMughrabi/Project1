f66e0cae54cab9afe207ec82a551e915abeab354
Nasser Mughrabi <abdalnasseralmughrabi@gmail.com>
2022-05-18 08:49:11 -0600

    entry, index, and search pages completed

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 
 from . import views
 
+app_name = "wiki"
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("search", views.search, name="search"),
+    path("new", views.new, name="new"),
+    path("random", views.random, name="random"),
+    path("<str:entry>", views.entry, name="entry")
 ]
--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,10 +1,54 @@
 from django.shortcuts import render
-
+from django import forms
 from . import util
 
+class newform(forms.Form):
+    entry_title = forms.CharField(label="New Task")
+
+def new(request):
+    return render(request, "encyclopedia/new.html", {
+        "form": newform()
+    })
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
+    })
+
+def new(request):
+    return render(request, "encyclopedia/new.html", {
+        "entries": util.list_entries()
+    })
+
+def random(request):
+    return render(request, "encyclopedia/random.html", {
+        "entries": util.list_entries()
+    })
+
+def search(request):
+    if request.GET.get('q', '') in util.list_entries():
+        return render(request, "encyclopedia/entry.html", {
+        "entry":  util.get_entry(request.GET.get('q', '')),
+        "entry_title": [request.GET.get('q', '')]
+    })
+
+    return render(request, "encyclopedia/search.html", {
+        "query_entry": request.GET.get('q', ''),
+        "entries": util.list_entries()
+    })
+
+
+def entry(request, entry):
+    if not entry.isupper():
+        entry = entry.capitalize()
+
+    if util.get_entry(entry) is None:
+        entry_result = "404 PAGE NOT FOUND"
+    else:
+        entry_result = util.get_entry(entry)
+
+    return render(request, "encyclopedia/entry.html", {
+        "entry": entry_result,
+        "title": entry
     })
 
